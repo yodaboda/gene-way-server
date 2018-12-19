67bc6eb794a0486c44f19523c44a48802b5f2f8a
Firas Swidan <frsswdn@gmail.com>
2018-12-19 15:54:46 +0200

    Formatting MeasurementUnitFormatTest

--- src/test/java/com/nutrinfomics/geneway/server/alerts/format/MeasurementUnitFormatTest.java
+++ src/test/java/com/nutrinfomics/geneway/server/alerts/format/MeasurementUnitFormatTest.java
@@ -1,6 +1,6 @@
 package com.nutrinfomics.geneway.server.alerts.format;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,22 +16,21 @@ public class MeasurementUnitFormatTest {
 
 	private ResourceBundles mockResourceBundles;
 	private MeasurementUnitFormat measurementUnitFormat;
-	
-	
+
 	@Before
 	public void setUp() {
 		mockResourceBundles = mock(ResourceBundles.class);
 		measurementUnitFormat = new MeasurementUnitFormat(mockResourceBundles);
 	}
-	
+
 	@Test
 	public void format_AsExpected() {
 		Locale locale = Locale.FRENCH;
 		MeasurementUnit measurementUnit = MeasurementUnit.LEAF;
-		
+
 		String resourceString = "German Leaf";
 		when(mockResourceBundles.getMeasurementResource(measurementUnit, locale)).thenReturn(resourceString);
-		
+
 		String formattedString = measurementUnitFormat.format(measurementUnit, locale);
 		assertEquals(resourceString, formattedString);
 	}
