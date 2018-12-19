3f323c2b56543b05b82610107a2b992260898c17
Firas Swidan <frsswdn@gmail.com>
2018-12-19 15:53:33 +0200

    Adding unit tests for MeasurementUnitFormat

---
+++ src/test/java/com/nutrinfomics/geneway/server/alerts/format/MeasurementUnitFormatTest.java
@@ -0,0 +1,39 @@
+package com.nutrinfomics.geneway.server.alerts.format;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.nutrinfomics.geneway.server.ResourceBundles;
+import com.nutrinfomics.geneway.shared.MeasurementUnit;
+
+public class MeasurementUnitFormatTest {
+
+	private ResourceBundles mockResourceBundles;
+	private MeasurementUnitFormat measurementUnitFormat;
+	
+	
+	@Before
+	public void setUp() {
+		mockResourceBundles = mock(ResourceBundles.class);
+		measurementUnitFormat = new MeasurementUnitFormat(mockResourceBundles);
+	}
+	
+	@Test
+	public void format_AsExpected() {
+		Locale locale = Locale.FRENCH;
+		MeasurementUnit measurementUnit = MeasurementUnit.LEAF;
+		
+		String resourceString = "German Leaf";
+		when(mockResourceBundles.getMeasurementResource(measurementUnit, locale)).thenReturn(resourceString);
+		
+		String formattedString = measurementUnitFormat.format(measurementUnit, locale);
+		assertEquals(resourceString, formattedString);
+	}
+
+}
