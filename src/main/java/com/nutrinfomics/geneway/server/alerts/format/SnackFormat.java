35283a48935dd55c1c04e8306e8ce443ec138374
Firas Swidan <frsswdn@gmail.com>
2018-12-20 12:45:31 +0200

    Refactoring and adding tests to SnackFormat

--- src/main/java/com/nutrinfomics/geneway/server/alerts/format/SnackFormat.java
+++ src/main/java/com/nutrinfomics/geneway/server/alerts/format/SnackFormat.java
@@ -23,51 +23,54 @@ public class SnackFormat {
     this.resourceBundles = resourceBundles;
   }
 
+  //TODO: Use decorator design pattern for classes in this package
   public String format(Snack snack, Locale locale) {
     if (snack instanceof GeneralVaryingSnack) {
       return formatGeneralVaryingSnack((GeneralVaryingSnack) snack, locale);
     }
     Collection<FoodItem> foodItems = snack.getFoodItems();
-    String s = "";
+    StringBuilder bld = new StringBuilder();
     for (FoodItem foodItem : foodItems) {
-      s += foodItemFormat.format(foodItem, locale) + " + ";
+      bld.append(foodItemFormat.format(foodItem, locale));
+      bld.append(" + ");
     }
-    return s.isEmpty() ? s : s.substring(0, s.length() - 2); // string ends with "+ "
+    String s = bld.toString();
+    return s.isEmpty() ? s : s.substring(0, s.length() - 3); // string ends with " + "
   }
 
   private String formatGeneralVaryingSnack(GeneralVaryingSnack snack, Locale locale) {
-    String res = "";
+    StringBuilder bld = new StringBuilder();
     for (Snack snck : snack.getSnacks()) {
       int minCycleLength = Integer.MAX_VALUE;
       for (FoodItem foodItem : snck.getFoodItems()) {
         minCycleLength = Math.min(minCycleLength, foodItem.getCycle().getCycleLength());
       }
-      res +=
-          format(snck, locale)
-              + " - "
-              + NumberFormat.getIntegerInstance(locale).format(minCycleLength)
-              + " "
-              + resourceBundles.getMiscBundleResource("days", locale)
-              + System.getProperty("line.separator");
+      bld.append(format(snck, locale));
+      bld.append(" - ");
+      bld.append(NumberFormat.getIntegerInstance(locale).format(minCycleLength));
+      bld.append(" ");
+      bld.append(resourceBundles.getMiscBundleResource("days", locale));
+      bld.append(System.getProperty("line.separator"));
     }
-    return res;
+    String s = bld.toString();
+    return s.isEmpty() ? s : s.substring(0, s.length() - 1); // string ends with "line.separator"
   }
 
   public String formatMeatSnack(Snack snack, Locale locale) {
     Collection<FoodItem> foodItems = snack.getFoodItems();
-    String s = "";
+    StringBuilder bld = new StringBuilder();
     for (FoodItem foodItem : foodItems) {
-      s +=
-          foodItemFormat.format(
-                  new FoodItem(
-                      foodItem.getAmount(), foodItem.getMeasurementUnit(), foodItem.getFoodType()),
-                  locale)
-              + " - "
-              + NumberFormat.getIntegerInstance(locale).format(foodItem.getCycle().getCycleLength())
-              + " "
-              + resourceBundles.getMiscBundleResource("days", locale)
-              + System.getProperty("line.separator");
+    	bld.append(          foodItemFormat.format(
+                new FoodItem(
+                        foodItem.getAmount(), foodItem.getMeasurementUnit(), foodItem.getFoodType()),
+                    locale));
+    	bld.append(" - ");
+    	bld.append(NumberFormat.getIntegerInstance(locale).format(foodItem.getCycle().getCycleLength()));
+    	bld.append(" ");
+    	bld.append(resourceBundles.getMiscBundleResource("days", locale));
+    	bld.append(System.getProperty("line.separator"));
     }
+    String s = bld.toString();
     return s.isEmpty() ? s : s.substring(0, s.length() - 1); // string ends with "\n"
   }
 }
