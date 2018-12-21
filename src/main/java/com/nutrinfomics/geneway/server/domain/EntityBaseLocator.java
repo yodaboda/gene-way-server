d47b7d906d62cc0116329b060073e546301b3a98
Firas Swidan <frsswdn@gmail.com>
2018-12-21 17:32:13 +0200

    Add logger to class

--- src/main/java/com/nutrinfomics/geneway/server/domain/EntityBaseLocator.java
+++ src/main/java/com/nutrinfomics/geneway/server/domain/EntityBaseLocator.java
@@ -2,50 +2,55 @@ package com.nutrinfomics.geneway.server.domain;
 
 import javax.persistence.EntityManager;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.web.bindery.requestfactory.shared.Locator;
 
 public class EntityBaseLocator extends Locator<EntityBase, Long> {
 
-  @Inject Provider<EntityManager> entityManager;
+	private static final Logger LOGGER = LogManager.getLogger();
 
-  @Override
-  public EntityBase create(Class<? extends EntityBase> clazz) {
-    try {
-      return clazz.newInstance();
-    } catch (InstantiationException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IllegalAccessException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return null;
-  }
+	@Inject
+	Provider<EntityManager> entityManager;
 
-  @Override
-  public EntityBase find(Class<? extends EntityBase> clazz, Long id) {
-    return entityManager.get().find(clazz, id);
-  }
+	@Override
+	public EntityBase create(Class<? extends EntityBase> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			LOGGER.log(Level.FATAL, e.toString(), e);
+		} catch (IllegalAccessException e) {
+			LOGGER.log(Level.FATAL, e.toString(), e);
+		}
+		return null;
+	}
 
-  @Override
-  public Class<EntityBase> getDomainType() {
-    throw new UnsupportedOperationException();
-  }
+	@Override
+	public EntityBase find(Class<? extends EntityBase> clazz, Long id) {
+		return entityManager.get().find(clazz, id);
+	}
 
-  @Override
-  public Long getId(EntityBase domainObject) {
-    return domainObject.getId();
-  }
+	@Override
+	public Class<EntityBase> getDomainType() {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override
-  public Class<Long> getIdType() {
-    return Long.class;
-  }
+	@Override
+	public Long getId(EntityBase domainObject) {
+		return domainObject.getId();
+	}
 
-  @Override
-  public Object getVersion(EntityBase domainObject) {
-    return domainObject.getVersion();
-  }
+	@Override
+	public Class<Long> getIdType() {
+		return Long.class;
+	}
+
+	@Override
+	public Object getVersion(EntityBase domainObject) {
+		return domainObject.getVersion();
+	}
 }
