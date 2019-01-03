521238603e40d366263c78178eefa4e6e7d89202
Firas Swidan <frsswdn@gmail.com>
2019-01-03 11:33:35 +0200

    Injecting locale directly to GuiceServiceLayerDecorator

--- src/main/java/com/nutrinfomics/geneway/server/requestfactory/GuiceServiceLayerDecorator.java
+++ src/main/java/com/nutrinfomics/geneway/server/requestfactory/GuiceServiceLayerDecorator.java
@@ -16,22 +16,21 @@ import com.google.web.bindery.requestfactory.shared.Locator;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.Service;
 import com.google.web.bindery.requestfactory.shared.ServiceLocator;
-import com.nutrinfomics.geneway.server.Utils;
 
 public class GuiceServiceLayerDecorator extends ServiceLayerDecorator {
   /** JSR 303 validator used to validate requested entities. */
   private final Validator validator;
 
   private final Injector injector;
-  private Utils utils;
+  private Locale requestLocale;
 
   @Inject
   protected GuiceServiceLayerDecorator(
-      final Injector injector, final Validator validator, Utils utils) {
+      final Injector injector, final Validator validator, Locale requestLocale) {
     super();
     this.injector = injector;
     this.validator = validator;
-    this.utils = utils;
+    this.requestLocale = requestLocale;
   }
 
   @Override
@@ -61,13 +60,11 @@ public class GuiceServiceLayerDecorator extends ServiceLayerDecorator {
   public <T> Set<ConstraintViolation<T>> validate(T domainObject) {
     ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     MessageInterpolator defaultInterpolator = factory.getMessageInterpolator();
-    Locale locale = utils.getLocale();
-    //				new Locale(RequestFactoryServlet
-    //				.getThreadLocalRequest().getHeader("X-GWT-Locale"));
+
     GeneWayLocaleMessageInterpolator interpolator =
-        new GeneWayLocaleMessageInterpolator(defaultInterpolator, locale);
-    Validator validator = factory.usingContext().messageInterpolator(interpolator).getValidator();
-    return validator.validate(domainObject);
-    //		return validator.validate(domainObject);
+        new GeneWayLocaleMessageInterpolator(defaultInterpolator, requestLocale);
+    Validator domainObjectValidator =
+        factory.usingContext().messageInterpolator(interpolator).getValidator();
+    return domainObjectValidator.validate(domainObject);
   }
 }
